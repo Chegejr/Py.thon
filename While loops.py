d20126ae922426b2439bb5fc875a315765684f9b
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-07-03 11:11:51 +0300

    While loops.py

--- While loops.py
+++ While loops.py
@@ -13,3 +13,28 @@ while i <= 5:
     i +=1
     print()
 
+# Use of continue in while loop
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+# Use of while loop in conjuction with else
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+# Breaking out of a while loop when a certain condition is met
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
