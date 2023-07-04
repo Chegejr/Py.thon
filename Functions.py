e005cc0da04a379b06c84a4fb9ee80d7079c57c9
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-07-04 22:01:47 +0300

    Functions.py

--- Functions.py
+++ Functions.py
@@ -88,3 +88,19 @@ def multiples(x):
 print(multiples(6))
 print(multiples(5))
 print(multiples(7))
+
+# Python also accepts recursion, this means a defined function can call itself
+# Recursion is a common mathematical and programming concept which has the benefits of looping through data to get a desired result
+# In this example, tri_recursion() is a function that we have defined to call itself ("recurse"). 
+# We use the k variable as the data, which decrements (-1) every time we recurse. 
+# The recursion ends when the condition is not greater than 0 (i.e. when it is 0)
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
