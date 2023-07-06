48324f79effef560d87a9c200d409bc91286c9e7
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-07-06 22:42:22 +0300

    Modules.py

--- Modules.py
+++ Modules.py
@@ -32,3 +32,16 @@ import platform
 x = dir(platform)
 print(x)
 
+def greeting(name):
+  print("Hello, " + name)
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+from mymodule import person1
+
+print (person1["age"])
+
