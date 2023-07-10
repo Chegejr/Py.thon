daa04daaa5fdfd069d85630da9b02cd1bf3479cc
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-07-10 23:48:12 +0300

    Working with JSON.py

--- Working with JSON.py
+++ Working with JSON.py
@@ -7,3 +7,27 @@
 # Create a program that reads this book
 # A tip! The address book will be inform a dictionary
 
+addressBook = {}
+addressBook["Malone"] = {
+    "name": "Past Malone",
+    "Address": "Baltimore34",
+    "Pager": 73108243
+}
+
+addressBook["michael"] = {
+    "name": "Michael Scofield",
+    "Address": "Fox River",
+    "Pager": 73164243
+}
+
+addressBook["ragnar"] = {
+    "name": "Ragnar Lothbrok",
+    "Address": "Kattegart",
+    "Pager": 38108463
+}
+
+addressBook["leo"] = {
+    "name": "Lionel Messi",
+    "Address": "Nou Camp",
+    "Pager": 10180243
+}
