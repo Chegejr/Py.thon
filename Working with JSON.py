ba9e691af72630b391d3a09f2cc70852f778074a
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-07-10 23:42:18 +0300

    Add files via upload

---
+++ Working with JSON.py
@@ -0,0 +1,9 @@
+# JSON is an acronym for Javascript Object Notation
+# It is a data exchange format just like xml
+# compared to XML JSON is more lightweight 
+
+# The problem
+# Creat an address book and add some records in it
+# Create a program that reads this book
+# A tip! The address book will be inform a dictionary
+
