6a86d589c73dbcd3ce3f2ca14754437dd95457fe
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-08-04 14:47:28 +0300

    NumPy.py

--- NumPy.py
+++ NumPy.py
@@ -240,3 +240,42 @@ arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16
 newarr = np.hsplit(arr, 3)
 print(newarr)
 # Note: Similar alternates to vstack() and dstack() are available as vsplit() and dsplit().
+
+# Searching Arrays
+# You can search an array for a certain value, and return the indexes that get a match.
+# To search an array, use the where() method.
+import numpy as np
+arr21= np.array([1,2,3,4,5,6,6,8,1,2])
+x= np.where(arr21== 1)
+print(x)
+
+# Find the indexes where the values are even:
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+x = np.where(arr%2 == 0)
+print(x)
+
+# Find the indexes where the values are odd:
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+x = np.where(arr%2 == 1)
+print(x)
+
+# Search sorted
+# Find the indexes where the value 7 should be inserted:
+import numpy as np
+arr = np.array([6, 7, 8, 9])
+x = np.searchsorted(arr, 7)
+print(x)
+
+# Find the indexes where the value 7 should be inserted, starting from the right:
+import numpy as np
+arr = np.array([6, 7, 8, 9])
+x = np.searchsorted(arr, 7, side='right')
+print(x)
+
+# Find the indexes where the values 2, 4, and 6 should be inserted:
+import numpy as np
+arr = np.array([1, 3, 5, 7])
+x = np.searchsorted(arr, [2, 4, 6])
+print(x)
