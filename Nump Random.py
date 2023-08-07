505215d7ad66d0058aad9cbad809297f7d6af068
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-08-07 13:11:46 +0300

    Nump Random.py

--- Nump Random.py
+++ Nump Random.py
@@ -206,3 +206,14 @@ print(y)
 from numpy import random
 y = random.pareto(a=3,size=(2,3))
 print(y)
+
+# Zipf distributions are used to sample data based on zipf's law.
+# Zipf's Law: In a collection, the nth common term is 1/n times of the most common term. 
+# E.g. the 5th most common word in English occurs nearly 1/5 times as often
+# takes in two parameters i.e
+# a = distribution parameter
+# size = shape of the returned array
+from numpy import random
+y = random.zipf(a=3,size=(2,3))
+print(y)
+
