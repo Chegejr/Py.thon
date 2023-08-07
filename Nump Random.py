98ec14496aa11ff1d23eb1f1a12ae938590f5b30
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-08-07 11:46:20 +0300

    Nump Random.py

--- Nump Random.py
+++ Nump Random.py
@@ -108,4 +108,21 @@ from numpy import random
 y = random.normal(size=(2,3),loc=1,scale=2)
 print(y)
 
+# Binomial Distribution
+# Binomial Distribution is a Discrete Distribution.
+# It describes the outcome of binary scenarios, e.g. toss of a coin, it will either be head or tails.
+# It has three parameters:
+# n - number of trials.
+# p - probability of occurence of each trial (e.g. for toss of a coin 0.5 each).
+# size - The shape of the returned array.
+# Discrete Distribution:The distribution is defined at separate set of events,
+# e.g. a coin toss's result is discrete as it can be only head or tails whereas height
+# Discrete Distribution:The distribution is defined at separate set of events,
+# e.g. a coin toss's result is discrete as it can be only head or tails whereas height of
+# people is continuous as it can be 170, 170.1, 170.11 and so on.
+
+from numpy import random
+x = random.binomial(n=5,p=0.5,size=12)
+print(x)
+# The main difference is that normal distribution is continous whereas binomial is discrete
 
