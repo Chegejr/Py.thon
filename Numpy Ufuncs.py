5bb2ef5198b4678ca40a3a77bed58ffd32ae46b1
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-08-08 10:40:37 +0300

    Numpy Ufuncs.py

--- Numpy Ufuncs.py
+++ Numpy Ufuncs.py
@@ -23,3 +23,28 @@ z = np.add(x,y)
 print(z)
 #  Now we can see that instead of iterating over each element in the array when adding we can simply use the ufunc add()
 #  to easily add the two lists together
+
+# creating your own ufuncs
+# create a normal function then add it to the numpy ufuncs library using the frompyfunc() method
+# the frompyfunc() takes 3 arguments i.e
+# function = name of the function
+# input = number of inputs
+# outpit = number of outputs
+def greetings(fname = "moon",sname= "jr"):
+    print("My name is",fname,sname)
+greetings()
+
+import numpy as np
+greetings = np.frompyfunc(greetings,2,1)
+print(greetings("moon","jr"))
+
+# 2nd example on creating your on ufunc
+def add(x,y):
+    sum = x+y
+    print("The total sum is:",sum)
+add(12,12)
+
+import numpy as np
+add = np.frompyfunc(add,2,1)
+print(add(12,15))
+    
