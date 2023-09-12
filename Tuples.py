1da4a8e6774882b63cf974b0864a8cba9b765be3
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-09-12 20:21:14 +0300

    Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -13,19 +13,17 @@ print(Tuple1); # Output-------->('Future', 21, 'Mia', 'Ugali', 'Banana', 'Ugali'
 print(tuple2); # Output-------->('Chege', 'Menengai', 89, ('Einstein', 78, 'Ugali'))
 print(Tuple1 + tuple2); # Output-------->('Future', 21, 'Mia', 'Ugali', 'Banana', 'Ugali', 78, 'Chege', 'Menengai', 89, ('Einstein', 78, 'Ugali'))
 
-print(Tuple1[0]); # Tuple slicing
-print(tuple2[-1][2]); # Tuple slicing a tuple inside another tuple
-print((Tuple1).count("Ugali")); # Counts how many times a specified item appears in the list
+print(Tuple1[0]);  # Output--------> Future
+print(tuple2[-1][2]); # slicing a tuple inside another tuple.  # Output--------> Ugali
+print((Tuple1).count("Ugali")); # Counts how many times a specified item appears in the list.  # Output--------> 2
 
-print(type(Tuple1)); # Checking if indeed what we have is a tuple
-tuple3  = print(("Ugali, ")* 4);
+print(type(Tuple1)); # Checking if indeed what we have is a tuple.  # Output--------> <class 'tuple'>
+tuple3  = print(("Ugali, ")* 4);  # Output--------> Ugali, Ugali, Ugali, Ugali,
 
 # Appending an item into  an already existing tuple
-
 c = list(tuple2);
 c.append("Added");
 tuple2 = tuple(c);
-
 print(tuple2);
 
 # Secoond example of appending an element to a tuple by first changing it into a list and back to tuple
@@ -45,7 +43,6 @@ print(type(tuple4));
 print(tuple4);
 
 # Manipulating tuples using "For Loops"
-
 for x in (tuple2):
     print(x);
     
