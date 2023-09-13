1da8059c38f9dd730ed57ff164c079198c7dbb40
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-09-13 12:21:14 +0300

    Add files via upload

---
+++ DataScience.py
@@ -0,0 +1,21 @@
+"""
+SQL For Data Science: https://bit.ly/3LRDgQV
+
+Google Advanced Data Analytics: https://bit.ly/3GqqYvG
+
+IBM Data Engineer: https://bit.ly/3rtKHpF
+
+DataCamp Data Analyst: https://bit.ly/3nwVU7o
+
+Machine Learning Course: https://bit.ly/3Z4kkDD
+
+Learn SQL: https://bit.ly/3r1ZVCs
+
+Learn Excel: https://bit.ly/3pNygVb
+
+Harvard Data Science (Python): https://bit.ly/45GAPs0
+
+IBM Data Science: https://bit.ly/3JOAOKd
+
+Google Data Analytics: https://bit.ly/3EK2ZH6
+"""
