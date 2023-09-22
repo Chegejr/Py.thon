ed2bb094b2eec4357534ffc46209d33b0a0b5630
Chegejr <133355257+Chegejr@users.noreply.github.com>
2023-09-22 04:46:50 +0300

    First Code.py

---
+++ First Code.py
@@ -0,0 +1,44 @@
+slip1 = {
+    "Match1":{
+        "Wolves":"Liverpool",
+        "pick":"Over 3.5",
+        "Country":"England",
+    },
+     "Match2":{
+        "Tottenham":"Sheffield Utd",
+        "pick":"Over 3.5",
+        "Country":"England",
+    },
+     "Match3":{
+        "WestHam":"Manchester city",
+        "pick":"Over 3.5",
+        "Country":"England",
+    },
+     "Match4":{
+        "Farenese":"Braga",
+        "pick":"Over 3.5",
+        "Country":"Netherlands",
+    },
+     "Match5":{
+        "Sturm Graz":"Rb Salzburg",
+        "pick":"Over 3.5",
+        "country":"Austria",
+    },
+     "Match6":{
+        "Club Brugge":"Royal Charleroi",
+        "pick":"Over 3.5",
+        "Country":"Belgium",
+    }
+}
+
+for x,y in slip1.items():
+    print(x,y)
+# Output---------->
+"""
+Match1 {'Wolves': 'Liverpool', 'pick': 'Over 3.5', 'Country': 'England'}
+Match2 {'Tottenham': 'Sheffield Utd', 'pick': 'Over 3.5', 'Country': 'England'}
+Match3 {'WestHam': 'Manchester city', 'pick': 'Over 3.5', 'Country': 'England'}
+Match4 {'Farenese': 'Braga', 'pick': 'Over 3.5', 'Country': 'Netherlands'}
+Match5 {'Sturm Graz': 'Rb Salzburg', 'pick': 'Over 3.5', 'country': 'Austria'}
+Match6 {'Club Brugge': 'Royal Charleroi', 'pick': 'Over 3.5', 'Country': 'Belgium'}
+"""
